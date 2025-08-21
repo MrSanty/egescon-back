9417e46194b22583c30aac493f960f711af8f5c1
MrSanty <sr.santyquinteror@gmail.com>
2025-08-21 14:27:17 -0500

    feat: remove Company and Roles modules from AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { CompanyModule } from './company/company.module';
-import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule, CompanyModule, RolesModule],
+  imports: [AuthModule, UsersModule, PrismaModule],
   providers: [PrismaService],
 })
 export class AppModule {}
