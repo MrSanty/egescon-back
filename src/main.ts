56acbeb2d9e3d20162eb6037e203040db76b80bd
MrSanty <sr.santyquinteror@gmail.com>
2025-08-21 10:59:38 -0500

    feat: enhance security by adding CORS, cookie parser, and helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+import { AccessTokenGuard } from './auth/guards/access-token.guard';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import cookieParser = require('cookie-parser');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  app.use(cookieParser());
+  app.use(helmet());
+
+  const reflector = new Reflector();
+  app.useGlobalGuards(new AccessTokenGuard(reflector));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
